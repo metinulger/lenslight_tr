52d1963479c27984a58360a599b33ca52a091b36
metinulger <123983513+metinulger@users.noreply.github.com>
2024-05-04 00:54:06 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import conn from './db.js';
+import pageRoute from "./routers/pageRoute.js";
+dotenv.config();
+// connection to the DB
+conn();
 
 const app=express() 
-const port  = 3000
-app.get("/",(req,res)=>{
-    res.send('index sayfası 2');
-})
+const port  = process.env.PORT;
+// ejs template engine 
+app.set('view engine','ejs');
+
+// static file middleware
+app.use(express.static('public'))
+
+app.use("/",pageRoute);
 app.listen(port,()=>{
-    console.log("port : $(port)")
-}    )
+    console.log(`port : ${port}`)
+})
+
